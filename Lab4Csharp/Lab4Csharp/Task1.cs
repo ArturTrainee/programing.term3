ca8629c193c1b406c39f6ae7c0c62fb930907dfb
Artur Stecenko <ar2rstecenk0@gmail.com>
2019-11-11 19:09:48 +0200

    Added lab4 impl using c# & winforms

---
+++ Lab4Csharp/Lab4Csharp/Task1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using static System.Console;
+
+namespace Lab4Csharp
+{
+    class Task1
+    {
+        static readonly string RESOURCES_PATH = Path.Combine(Path.GetDirectoryName(
+            Assembly.GetExecutingAssembly().Location), @"Resources");
+
+        static void Main(string[] args)
+        {
+            List<string> fileNames = AddTrackedFileNames();
+            List<long> validPairProducts = new List<long>();
+            int[] pairValues = new int[2];
+            string line;
+            try
+            {
+                using (StreamWriter notFoundedFilesTracker = new StreamWriter(
+                    File.Open(RESOURCES_PATH + "\\" + "no_file.txt", FileMode.Create)))
+                using (StreamWriter invalidFilesTracker = new StreamWriter(
+                    File.Open(RESOURCES_PATH + "\\" + "bad_data.txt", FileMode.Create)))
+                using (StreamWriter overflowTypeFilesTracker = new StreamWriter(
+                    File.Open(RESOURCES_PATH + "\\" + "overflow.txt", FileMode.Create)))
+                {
+                    for (int i = 0; i < fileNames.Count; i++)
+                    {
+                        try
+                        {
+                            using (StreamReader streamReader = new StreamReader(
+                                File.Open(RESOURCES_PATH + "\\" + fileNames[i], FileMode.Open)))
+                            {
+                                line = streamReader.ReadLine();
+                                pairValues[0] = int.Parse(line);
+                                line = streamReader.ReadLine();
+                                pairValues[1] = int.Parse(line);
+                            }
+                        }
+                        catch (FileNotFoundException ex)
+                        {
+                            notFoundedFilesTracker.Write(fileNames[i] + " ");
+                            WriteLine(ex.Message);
+                            continue;
+                        }
+                        catch (Exception ex) when (ex is FormatException || ex is ArgumentNullException)
+                        {
+                            invalidFilesTracker.Write(fileNames[i] + " ");
+                            WriteLine(ex.Message);
+                            continue;
+                        }
+                        catch (OverflowException ex)
+                        {
+                            overflowTypeFilesTracker.Write(fileNames[i] + " ");
+                            WriteLine(ex.Message);
+                            continue;
+                        }
+                        validPairProducts.Add(pairValues[0] * pairValues[1]);
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                WriteLine(ex.Message);
+            }
+            PrintStatistics(validPairProducts);
+            ReadKey();
+        }
+
+        private static List<string> AddTrackedFileNames()
+        {
+            List<string> fileNames = new List<string>(19);
+            for (int i = 10; i < 30; i++) fileNames.Add(i + ".txt");
+            return fileNames;
+        }
+
+        private static void PrintStatistics(List<long> validPairProducts)
+        {
+            WriteLine("Products: ");
+            for (int i = 0; i < validPairProducts.Count; i++)
+            {
+                Write(validPairProducts[i] + "\t");
+            }
+            WriteLine("\nProducts average: " + (validPairProducts.Count > 0 ? validPairProducts.Average() : 0));
+            WriteLine("Sum of all valid pair products: " + validPairProducts.Sum());
+        }
+    }
+}
---
+++ Lab4Csharp/Lab4Task2/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Media.Imaging;
+
+namespace Lab4Task2
+{
+    public partial class Form1 : Form
+    {
+        static string RESOURCES_PATH = Path.Combine(Path.GetDirectoryName(
+            Assembly.GetExecutingAssembly().Location), @"Resources");
+
+        static Regex regexForImage = new Regex("^(.)((bmp)|(gif)|(tiff?)|(jpe?g)|(png))$",
+            RegexOptions.IgnoreCase);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            var dirInfo = new DirectoryInfo(RESOURCES_PATH);
+            var files = dirInfo.GetFiles("*.*");
+            var images = new Dictionary<Image, FileInfo>();
+            foreach (var file in files)
+            {
+                try
+                {
+                    if (regexForImage.IsMatch(file.Extension))
+                    {
+                        images.Add(Image.FromFile(file.FullName), file);
+                    }
+                }
+                catch (OutOfMemoryException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            foreach (Bitmap bitmapImage in images.Keys)
+            {
+                bitmapImage.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                var frame = BitmapFrame.Create(ConvertBitmap(bitmapImage));
+                var encoder = new GifBitmapEncoder();
+                encoder.Frames.Add(frame);
+                images.TryGetValue(bitmapImage, out FileInfo file);
+                string imgPath = Path.Combine(file.DirectoryName, file.Name.Split('.')[0] + "-mirrored.gif");
+                try
+                {
+                    using (var fs = new FileStream(imgPath, FileMode.Create))
+                    {
+                        encoder.Save(fs);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        public static BitmapSource ConvertBitmap(Bitmap source)
+        {
+            return System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                          source.GetHbitmap(),
+                          IntPtr.Zero,
+                          Int32Rect.Empty,
+                          BitmapSizeOptions.FromEmptyOptions());
+        }
+    }
+}
